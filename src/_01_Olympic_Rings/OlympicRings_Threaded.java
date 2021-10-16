9e4f3830a474ad3bd2926d7801089d25f7b525d7
Meme1sh_ <60296686+The-GitHub-Paradox@users.noreply.github.com>
2021-10-16 14:26:49 -0700

    e
    e

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,8 +1,39 @@
 package _01_Olympic_Rings;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
-public class OlympicRings_Threaded {
-	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
+import javax.swing.JFrame;
 
+import org.jointheleague.graphical.robot.Robot;
+
+public class OlympicRings_Threaded{
+	public static void main(String[] args) {
+		Robot[] trobots = new Robot[3];
+		Robot[] brobots = new Robot[2];
+		int x = 250;
+		int y = 300;
+		for (int i = 0; i < trobots.length; i++) {
+			trobots[i] = new Robot();
+			trobots[i].setX(x + (i*75));
+			trobots[i].setY(y);
+			for(int r = 0; r < 180; r++) {
+				trobots[i].penDown();
+				trobots[i].move(2);
+				trobots[i].turn(2);
+			}
+		}
+		for (int i = 0; i < brobots.length; i++) {
+			brobots[i] = new Robot();
+			brobots[i].setX(x + 18 + (i*75));
+			brobots[i].setY(y + 35);
+			for(int r = 0; r < 180; r++) {
+				brobots[i].penDown();
+				brobots[i].move(2);
+				brobots[i].turn(2);
+			}
+		}
+		
+	}
 }
 
--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -1,6 +1,53 @@
 package _02_Advanced_Robot_Race;
 
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class AdvancedRobotRace {
-	// Re-do the robot race recipe from level 3 module 0. 
-	// This time, use threads to make all of the robots go at the same time.
+
+	public static void main(String[] args) {
+		
+		Robot[] robots = new Robot[5];
+			
+		boolean b = true;
+			
+		int x = 250;
+		int y = 500;
+		for (int i = 0; i < robots.length; i++) {
+			robots[i] = new Robot();
+			robots[i].setX(x + (i*100));
+			robots[i].setY(y);
+		}
+		String sr = JOptionPane.showInputDialog("What is the maximum distance you want the robots to go? (Enter an Integer)");
+		int r = Integer.parseInt(sr);
+		while(b) {
+			for(int i = 0; i < robots.length; i++) {
+				int ran1 = new Random().nextInt(r);
+				int ran2 = new Random().nextInt(r);
+				int ran3 = new Random().nextInt(r);
+				int ran4 = new Random().nextInt(r);
+				int ran5 = new Random().nextInt(r);
+				Thread r1 = new Thread(()->robots[0].move(ran1));
+				Thread r2 = new Thread(()->robots[1].move(ran2));
+				Thread r3 = new Thread(()->robots[2].move(ran3));
+				Thread r4 = new Thread(()->robots[3].move(ran4));
+				Thread r5 = new Thread(()->robots[4].move(ran5));
+				r1.start();
+				r2.start();
+				r3.start();
+				r4.start();
+				r5.start();
+				if(robots[i].getY() < 0) {
+					b = false;
+				}
+			}
+		}
+		
+		JOptionPane.showMessageDialog(null, "A robot has won!");
+	}
 }
+
+
